768523c0e1eec9a71649108e978d232caab068fe
Hsan-Kh <khecharemhsan@gmail.com>
2025-11-08 10:58:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -184,24 +184,24 @@ def main():
     
     st.sidebar.markdown("---")
     
-    col1, col2, col3 = st.sidebar.columns([1, 2, 1])
-    with col2:
-        if st.button("🚀 LANCER LA SIMULATION", type="primary", use_container_width=True):
-            # Créer params_dict explicitement avec les variables dont vous avez besoin
-            params_dict = {}
-            
-            # Récupérer seulement les variables de paramètres que vous utilisez
-            if 'temperature' in locals(): params_dict['temperature'] = temperature
-            if 'taux_refroidissement' in locals(): params_dict['taux_refroidissement'] = taux_refroidissement
-            if 'taille_tabou' in locals(): params_dict['taille_tabou'] = taille_tabou
-            if 'taille_population' in locals(): params_dict['taille_population'] = taille_population
-            if 'taux_croisement' in locals(): params_dict['taux_croisement'] = taux_croisement
-            if 'taux_mutation' in locals(): params_dict['taux_mutation'] = taux_mutation
-            if 'selection' in locals(): params_dict['selection'] = selection
-            if 'croisement' in locals(): params_dict['croisement'] = croisement
-            if 'mutation' in locals(): params_dict['mutation'] = mutation
-            
-            executer_simulation(probleme, algorithme, iterations, params_dict)
+        col1, col2, col3 = st.sidebar.columns([1, 2, 1])
+        with col2:
+            if st.button("🚀 LANCER LA SIMULATION", type="primary", use_container_width=True):
+                # Créer params_dict explicitement avec les variables dont vous avez besoin
+                params_dict = {}
+                
+                # Récupérer seulement les variables de paramètres que vous utilisez
+                if 'temperature' in locals(): params_dict['temperature'] = temperature
+                if 'taux_refroidissement' in locals(): params_dict['taux_refroidissement'] = taux_refroidissement
+                if 'taille_tabou' in locals(): params_dict['taille_tabou'] = taille_tabou
+                if 'taille_population' in locals(): params_dict['taille_population'] = taille_population
+                if 'taux_croisement' in locals(): params_dict['taux_croisement'] = taux_croisement
+                if 'taux_mutation' in locals(): params_dict['taux_mutation'] = taux_mutation
+                if 'selection' in locals(): params_dict['selection'] = selection
+                if 'croisement' in locals(): params_dict['croisement'] = croisement
+                if 'mutation' in locals(): params_dict['mutation'] = mutation
+                
+                executer_simulation(probleme, algorithme, iterations, params_dict)
     
     
     
@@ -375,6 +375,7 @@ def afficher_resultats_ordonnancement(resultats, algorithme):
 if __name__ == "__main__":
 
     main()
+
 
 
 
